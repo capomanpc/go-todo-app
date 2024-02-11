20f41db7d544b4557497a72064fe66a3355159fd
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-11 22:59:14 +0900

    chore: configファイルの作成と読み込みの実装

---
+++ config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+
+	"gopkg.in/go-ini/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	SQLDriver string
+	DBName    string
+	LogFile   string
+}
+
+var Config ConfigList
+
+// main関数より前に実行
+func init() {
+	LoadConfig()
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("coonfig.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("Port").MustString("8080"),
+		SQLDriver: cfg.Section("web").Key("SQLDriver").String(),
+		DBName:    cfg.Section("web").Key("DBName").String(),
+		LogFile:   cfg.Section("web").Key("Logfile").String(),
+	}
+}
