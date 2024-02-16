e902bceb2a959725b6c407f7fbf6e9c5e8114cdd
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-16 23:16:28 +0900

    feat:todoのUPDATE処理の実装

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/capomanpc/go-todo-app/app/models"
 )
 
@@ -10,15 +8,17 @@ func main() {
 	// log.Println("test")
 	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
-
 	/*
+		u := &models.User{}
+		u.Name = "test2"
+		u.Email = "test2@example.com"
+		u.PassWord = "testtest"
+		fmt.Println(u)
+
+		u.CreateUser()
+		/*
+
+		/*
 			u, _ := models.GetUser(1)
 
 			u.Name = "Test2"
@@ -32,13 +32,15 @@ func main() {
 			fmt.Println(u)
 
 
-		user, _ := models.GetUser(2)
-		user.CreateTodo("Firt Todo")
+			user, _ := models.GetUser(2)
+			user.CreateTodo("Firt Todo")
 	*/
+
 	/*
 		t, _ := models.GetUser(1)
 		fmt.Println(t)
 	*/
+
 	/*
 		user, _ := models.GetUser(2)
 		user.CreateTodo("Second Todo")
@@ -49,13 +51,19 @@ func main() {
 		}
 	*/
 
-	user, _ := models.GetUser(3)
-	user.CreateTodo("Third Todo")
+	/*
+		user, _ := models.GetUser(3)
+		user.CreateTodo("Third Todo")
 
-	user2, _ := models.GetUser(2)
-	todos, _ := user2.GetTodosByUser()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+		user2, _ := models.GetUser(2)
+		todos, _ := user2.GetTodosByUser()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	t, _ := models.GetTodo(1)
+	t.Content = "Update Todo"
+	t.UpdateTodo()
 
 }
