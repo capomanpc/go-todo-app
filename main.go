874f33f6a1c48ebecff3556f58dd4e211c2b87a5
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-16 21:15:37 +0900

    feat:todoテーブルからのデータ取得関数の実装

--- main.go
+++ main.go
@@ -10,29 +10,42 @@ func main() {
 	// log.Println("test")
 	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-
-	u.CreateUser()
-
 	/*
-		u, _ := models.GetUser(1)
-
-		u.Name = "Test2"
-		u.Email = "Test2@example.com"
-		u.UpdateUser()
-		u, _ = models.GetUser(1)
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.PassWord = "testtest"
 		fmt.Println(u)
 
-		u.DeleteUser()
-		u, _ = models.GetUser(1)
-		fmt.Println(u)
+		u.CreateUser()
+
+		/*
+			u, _ := models.GetUser(1)
+
+			u.Name = "Test2"
+			u.Email = "Test2@example.com"
+			u.UpdateUser()
+			u, _ = models.GetUser(1)
+			fmt.Println(u)
+
+			u.DeleteUser()
+			u, _ = models.GetUser(1)
+			fmt.Println(u)
+
+
+		user, _ := models.GetUser(2)
+		user.CreateTodo("Firt Todo")
+	*/
+	/*
+		t, _ := models.GetUser(1)
+		fmt.Println(t)
 	*/
 
 	user, _ := models.GetUser(2)
-	user.CreateTodo("Firt Todo")
+	user.CreateTodo("Second Todo")
 
+	todos, _ := models.GetTodos()
+	for _, v := range todos {
+		fmt.Println(v)
+	}
 }
