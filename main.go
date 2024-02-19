d6ea5e7af082549c2332edb401467895a3822bff
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-20 03:50:27 +0900

    feat:hハンドラの追加とhtmlファイル周りの実装

--- main.go
+++ main.go
@@ -3,49 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/capomanpc/go-todo-app/app/controllers"
 	"github.com/capomanpc/go-todo-app/app/models"
 )
 
 func main() {
-	// log.Println("test")
-	// fmt.Println(models.Db)
+	fmt.Println(models.Db)
 
-	/*
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
-
-		u.CreateUser()
-
-		/*
-			u, _ := models.GetUser(1)
-
-			u.Name = "Test2"
-			u.Email = "Test2@example.com"
-			u.UpdateUser()
-			u, _ = models.GetUser(1)
-			fmt.Println(u)
-
-			u.DeleteUser()
-			u, _ = models.GetUser(1)
-			fmt.Println(u)
-
-
-		user, _ := models.GetUser(2)
-		user.CreateTodo("Firt Todo")
-	*/
-	/*
-		t, _ := models.GetUser(1)
-		fmt.Println(t)
-	*/
-
-	user, _ := models.GetUser(2)
-	user.CreateTodo("Second Todo")
-
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+	controllers.StartMainServer()
 }
