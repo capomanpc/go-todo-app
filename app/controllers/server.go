2493b54b7491ae7a93ae5ce28b115a3739e1e8fe
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-26 00:25:36 +0900

    feat:HTMLファイルの追加とそれに伴うハンドラの設定

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -21,5 +21,7 @@ func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static", files))
 	http.HandleFunc("/", top)
+	http.HandleFunc("/signup", signup)
+	fmt.Println("http://localhost:8080")
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
