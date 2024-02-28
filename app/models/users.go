201d2ac0bd84e5f06d3a7b8124a72616ef841aff
capomanpc <121809222+capomanpc@users.noreply.github.com>
2024-02-29 07:27:27 +0900

    fix:前のコミットのバグ修正

--- app/models/users.go
+++ app/models/users.go
@@ -14,11 +14,11 @@ type User struct {
 	CreatedAt time.Time
 }
 type Session struct {
-	ID         int
-	UUID       string
-	Email      string
-	UserID     int
-	Created_at time.Time
+	ID        int
+	UUID      string
+	Email     string
+	UserID    int
+	CreatedAt time.Time
 }
 
 func (u *User) CreateUser() (err error) {
@@ -84,10 +84,10 @@ func GetUserByEmail(email string) (user User, err error) {
 		id,
 		uuid,
 		name,
-		emali,
+		email,
 		password,
 		created_at 
-	from users where email where email = ?`
+	from users where email = ?`
 	err = Db.QueryRow(cmd, email).Scan(
 		&user.ID,
 		&user.UUID,
@@ -97,4 +97,51 @@ func GetUserByEmail(email string) (user User, err error) {
 		&user.CreatedAt)
 
 	return user, err
+}
+
+func (u *User) CreateSession() (session Session, err error) {
+	session = Session{}
+	cmd1 := `insert into sessions (
+		uuid,
+		email,
+		user_id,
+		created_at) values (?,?,?,?)`
+
+	_, err = Db.Exec(cmd1, createUUID(), u.Email, u.ID, time.Now())
+	if err != nil {
+		log.Println(err)
+	}
+
+	cmd2 := `select id,uuid,email,user_id,created_at 
+		from sessions where user_id = ? and email = ?`
+
+	err = Db.QueryRow(cmd2, u.ID, u.Email).Scan(
+		&session.ID,
+		&session.UUID,
+		&session.Email,
+		&session.UserID,
+		&session.CreatedAt)
+
+	return session, err
+}
+
+func (sess *Session) CheckSession() (valid bool, err error) {
+	cmd := `select id,uuid,email,user_id,created_at from sessions where uuid = ?`
+
+	err = Db.QueryRow(cmd, sess.UUID).Scan(
+		&sess.ID,
+		&sess.UUID,
+		&sess.Email,
+		&sess.UserID,
+		&sess.CreatedAt)
+
+	if err != nil {
+		valid = false
+		return
+	}
+
+	if sess.ID != 0 {
+		valid = true
+	}
+	return valid, err
 }
